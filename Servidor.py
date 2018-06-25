1ed46a014beabb9fd4576c59446e3648938fee71
Nicolas Pavon <nicolaspavon33@gmail.com>
2018-06-25 20:03:00 -0300

    Delete Servidor.py
    LO BORRE

--- Servidor.py
+++
@@ -1,7 +0,0 @@
-import cgitb
-cgitb.enable() # Nos permite ver errores
-
-pagina = open(r"C:\Users\estudiante.fit\Desktop\Obligatorio\prueba.txt")
-for linea in pagina:
-    print(linea)
-pagina.close()
